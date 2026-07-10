a2b9d8c757840b7efd769c5a1c1272f0a7fead0c
Anastasia Yarosh <fysyad@gmail.com>
2026-07-10 09:50:39 +0300

    Вынесла иконку меню

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import ThemeChanger from "./DarkSwitch";
 import { Disclosure } from "@headlessui/react";
 import { DisclosureButton } from "@headlessui/react";
 import React, { useState } from "react";
+import { MenuIcon } from "./icons/MenuIcon";
 
 export const Navbar: FC = () => {
   const navigation = [
@@ -101,25 +102,7 @@ export const Navbar: FC = () => {
                   aria-label="Toggle Menu"
                   className="px-2 py-1 text-gray-500 rounded-md lg:hidden"
                 >
-                  <svg
-                    className="w-6 h-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    {open && (
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                      />
-                    )}
-                    {!open && (
-                      <path
-                        fillRule="evenodd"
-                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                      />
-                    )}
-                  </svg>
+                  <MenuIcon open={open} className="w-6 h-6" />
                 </DisclosureButton>
 
                 {/* <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden ">
