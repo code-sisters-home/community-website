6419581cb8d60b8bd53033a7288be8f7812cde0f
Anastasia Yarosh <fysyad@gmail.com>
2026-07-31 15:23:03 +0300

    Начала оптимизацию сайта, подгрузка изображения

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -27,7 +27,6 @@ export const About = () => {
 
   return (
     <Container className="flex flex-col-reverse lg:flex-wrap lg:flex-row max-w-7xl">
-      {/* Left section (Text) */}
       <div className="flex items-center w-full">
         <div className="max-w-7xl">
           {aboutContent.length > 0 ? (
@@ -36,19 +35,22 @@ export const About = () => {
                 <span className="purple">O нас</span>
               </h1>
               <div className="basic my-6 widget">
-                {/* Image in the top-right corner */}
+                {/* Image with optimized loading */}
                 <Image
                   src={adaImg}
                   alt="Ada Image"
-                  width="500"
-                  height="500"
-                  className="rounded-full w-full lg:w-1/3 object-cover p-6 lg:float-right" // mr-20 mt-20 - Ensure image floats and has some margin
+                  width={500}
+                  height={500}
+                  className="rounded-full w-full lg:w-1/3 object-cover p-6 lg:float-right"
+                  priority // Вместо fetchpriority для next/image
+                  sizes="(max-width: 1024px) 100vw, 33vw"
+                  loading="eager" // Явно указываем eager загрузку
                 />
                 <Markdown content={aboutContent} />
               </div>
             </>
           ) : (
-            <div className="text-[40px]">Загрузка постов...</div> // Или другой лоадер
+            <div className="text-[40px]">Загрузка постов...</div>
           )}
         </div>
       </div>
